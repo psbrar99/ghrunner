818e8dc2866e381a37ffe530d65dbc9405e02cf5
Prabhjot Singh <86856448+psbrar99@users.noreply.github.com>
2022-06-28 16:56:53 -0600

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	if Sum(2, 3, 4, 6, 9) != 54 {
+	if Sum(2, 3, 4, 6, 9) != 24 {
 		t.Error("expected", 24, "got", Sum(2, 3, 4, 6, 9))
 	}
 
