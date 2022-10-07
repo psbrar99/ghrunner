276e549a2cd237461e63369a63c1ab527ebcd368
psbrar99 <brargg1989@gmail.com>
2022-10-07 12:02:17 -0600

    scanner

---
+++ pull.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+# set -x
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+IMAGES=(app
+operator
+pilot
+proxyv2)
+
+IMAGE_SUFFIXES=("" "-debug" "-distroless")
+
+for image in "${IMAGES[@]}"; do
+  for suffix in "${IMAGE_SUFFIXES[@]}"; do
+    echo $HUB/${image}:${TAG}-${suffix} >> list.txt
+    docker pull $HUB/${image}:${TAG}-${suffix}
+  done
+done
