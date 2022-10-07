57c1d8056f159ee7e37b8238d5c961f52f12e11d
psbrar99 <brargg1989@gmail.com>
2022-10-07 11:25:11 -0600

    need var

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Sum(2, 3, 4, 6, 9))
+	fmt.Println(Sum(2, 3, 4, 6, 9,100))
 	fmt.Println(Sum(24, 43, 4, 66, 9))
 	fmt.Println(Sum(22222, 355, 400, 6, 9))
 
